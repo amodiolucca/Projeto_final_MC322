ab4b63ba63146af2a4bcf2904a8ee6aa62e8decd
Vítor Paziam Magalhães <v238316@dac.unicamp.br>
2023-06-21 09:53:23 -0300

    Adiciona versão inicial da GUI

---
+++ Projeto_final/src/projeto/classes/AtleticaGUI.java
@@ -0,0 +1,11 @@
+public class AtleticaGUI {
+
+    public static void main(String args[]) {
+        
+        Login loginFrame = new Login();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+    }
+    
+}
